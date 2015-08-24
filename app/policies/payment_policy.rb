322ecd3016138461650dbe972fd9709bf1615b87
Alex223124 <vzadnicy@yandex.ru>
2015-08-24 13:28:17 +0300

    Add Payment policy, works

--- app/policies/payment_policy.rb
+++ app/policies/payment_policy.rb
@@ -1,4 +1,5 @@
 class PaymentPolicy
+
 	attr_reader :current_user, :model 
 
 	def initialize(current_user, model)
@@ -6,24 +7,44 @@ class PaymentPolicy
 		@user = model
 	end
 
-
+	# Администратор может проматривать список платежей пользователя
+	# Пользователь может проматривать список всех своих платежей по кредиту(-ам)
 	def index?
-		@current_user.admin?
+		@current_user.admin? || @current_user == @user
 	end
 
+	# Только пользователь может делать новые платежи
+	def new?
+		@current_user == @user
+	end
+
+	def create?
+		@current_user == @user
+	end
+
+	# Администратор может проматривать детали конкретного платежа
+	# Юзер может просматривать детали конкретного платежи
 	def show?
 		@current_user.admin? || @current_user == @user
+	end
+
+	# Только администратор может редактировать данные сделанного платежа
+	def edit?
+		@current_user.admin?
 	end
 
 	def update?
 		@current_user.admin?
 	end
 
-	def destroy
-		return false if @current_user == @user
+	# Только админ может удалять платежи
+	def delete?
 		@current_user.admin?
 	end
-	
-	
+
+	def destroy?
+		@current_user.admin?
+	end
+
 
 end
